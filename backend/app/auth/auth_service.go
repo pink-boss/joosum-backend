465f54bc3bed149b7dd8bf16f9bdfdd0f4347b43
gitdog01 <hostdog01@naver.com>
2023-05-06 09:16:51 +0900

    feat(config) : oauth config

--- backend/app/auth/auth_service.go
+++ backend/app/auth/auth_service.go
@@ -3,7 +3,7 @@ package auth
 import (
 	"context"
 	"fmt"
-	"os"
+	"joosum-backend/pkg/config"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -11,8 +11,8 @@ import (
 
 var (
 	googleOauthConfig = &oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		ClientID:     config.GetEnvConfig("googleClientID"),
+		ClientSecret: config.GetEnvConfig("googleClientSecret"),
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
 		Endpoint:     google.Endpoint,
 	}

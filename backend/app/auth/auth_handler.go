00fdf54d38b66c20ea1ce396245df4b3c57b8f90
Hyunjun <kor.connor@gmail.com>
2023-06-02 11:57:15 +0900

    feat(login) : 메서드 추출해서 usecase 로 이동

--- backend/app/auth/auth_handler.go
+++ backend/app/auth/auth_handler.go
@@ -1,14 +1,9 @@
 package auth
 
 import (
-	"crypto/rsa"
-	"encoding/base64"
-	"encoding/binary"
-	"github.com/go-resty/resty/v2"
-	"github.com/golang-jwt/jwt/v4"
+	"fmt"
 	"joosum-backend/app/user"
 	"joosum-backend/pkg/util"
-	"math/big"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -46,61 +41,24 @@ func (h AuthHandler) SignUp(c *gin.Context) {
 
 	var userInfo user.User
 
-	if req.Social == "apple" {
-		pubKey := ApplePublicKey{}
-		publicSecret := PublicSecret{}
-		client := resty.New()
-
-		// 애플이 제공하는 public key 들을 가져옴
-		pubKeyResult, err := client.R().SetResult(&pubKey).Get(ApplePublicKeyURL)
-		JWKS := pubKeyResult.Result().(*ApplePublicKey)
-
-		if err != nil {
-			//return nil, fmt.Errorf("get apple public key err: %v", err)
-			return
-		}
-
-		claims := jwt.MapClaims{}
-		_, err = jwt.ParseWithClaims(req.AccessToken, &claims, func(token *jwt.Token) (interface{}, error) {
-			// kid 값 저장 | public key 대조에 필요하기 때문에
-			kid := token.Header["kid"].(string)
-
-			for _, v := range JWKS.Keys {
-				// 받아온 public key 중 id_token 과 kid 일치하는지 확인 후 n, e 값 저장
-				if kid == v.Kid {
-					n, _ := base64.RawURLEncoding.DecodeString(v.N)
-					publicSecret.N = n
-					e, _ := base64.StdEncoding.DecodeString(v.E)
-					publicSecret.E = e
-					break
-				}
-			}
-			publicKeyE := binary.LittleEndian.Uint32(append(publicSecret.E, 0))
-
-			// 이 rsaKey 로 id_token verify
-			rsaKey := &rsa.PublicKey{
-				N: new(big.Int).SetBytes(publicSecret.N),
-				E: int(publicKeyE),
-			}
-			return rsaKey, nil
-		})
-
-		email := claims["email"].(string)
-		userInfo.Email = email
-	} else {
-		userInfo.Email = req.Email
+	email, err := h.authUsecase.GetEmailFromJWT(req.Social, req.AccessToken)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, util.APIError{Error: fmt.Sprintf("failed to get email from the JWT token: %v", err.Error())})
+		return
 	}
 
+	userInfo.Email = email
 	userInfo.Social = req.Social
 	userInfo.Name = req.Nickname
 	userInfo.Age = req.Age
 	userInfo.Gender = req.Gender
 
-	_, err := h.authUsecase.SignUp(userInfo)
+	temp, err := h.authUsecase.SignUp(userInfo)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, util.APIError{Error: err.Error()})
 		return
 	}
+	print(temp)
 
 	accessToken, refreshToken, err := h.authUsecase.GenerateNewJWTToken([]string{"USER", "ADMIN"}, req.Email)
 

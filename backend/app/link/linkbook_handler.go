17515d797a258c79f08a1041f3a8dddbe4ef6f06
Hyunjun <kor.connor@gmail.com>
2023-06-19 19:17:12 +0900

    JOO-77 feat(link-book) : 링크북 등록 api 작성
    1. BackgroundColor 와 TitleColor, CreatedAt, UpdatedAt 추가
    2. DB 에 스네이크 케이스로 저장
    3. userId 를 context 에서 가져오도록 작성
    4. 응답코드 200 -> 201 수정
    5. req, res 분리

--- backend/app/link/linkbook_handler.go
+++ backend/app/link/linkbook_handler.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"github.com/gin-gonic/gin"
+	"joosum-backend/app/user"
 	"joosum-backend/pkg/util"
 	"net/http"
 )
@@ -13,22 +14,32 @@ type LinkBookHandler struct {
 // CreateLinkBook
 // @Tags 링크
 // @Summary 링크북 생성
-// @Param request body link.LinkBook true "request"
-// @Success 200 {object} link.LinkBookResponse
+// @Param request body link.LinkBookReq true "request"
+// @Success 200 {object} link.LinkBookRes
+// @Security ApiKeyAuth
 // @Router /link-books [post]
 func (h LinkBookHandler) CreateLinkBook(c *gin.Context) {
-	var req LinkBook
+	var req LinkBookReq
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, util.APIError{Error: "Invalid request body"})
 		return
 	}
 
-	res, err := h.linkUsecase.CreateLinkBook(req)
+	currentUser, exists := c.Get("user_id")
+	if !exists {
+		// 401 Unauthorized
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing Authorization header"})
+		return
+	}
+
+	userId := currentUser.(*user.User).UserId
+
+	res, err := h.linkUsecase.CreateLinkBook(req, userId)
 	if err != nil {
 		// 500 Internal Server Error
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, res)
+	c.JSON(http.StatusCreated, res)
 }
--- backend/app/link/linkbook_model.go
+++ backend/app/link/linkbook_model.go
@@ -2,26 +2,41 @@ package link
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"joosum-backend/pkg/db"
 	"time"
 )
 
 type LinkModel struct{}
 
-type LinkBook struct {
-	Name         string `json:"name"`
-	Color        string `json:"color"`
-	Illustration string `json:"illustration"`
+type LinkBookReq struct {
+	Title           string `json:"title"`
+	BackgroundColor string `json:"backgroundColor"`
+	TitleColor      string `json:"titleColor"`
+	Illustration    string `json:"illustration"`
 }
 
-func (LinkModel) CreateLinkBook(req LinkBook) (*LinkBook, error) {
+type LinkBookRes struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id" example:"649028fab77fe1a8a3b0815e"`
+	Title           string             `bson:"title" json:"title"`
+	BackgroundColor string             `bson:"background_color" json:"backgroundColor"`
+	TitleColor      string             `bson:"title_color" json:"titleColor"`
+	Illustration    string             `bson:"illustration" json:"illustration"`
+	CreatedAt       time.Time          `bson:"created_at" json:"-"`
+	UpdatedAt       time.Time          `bson:"updated_at" json:"-"`
+	UserId          string             `bson:"user_id" example:"User-0767d6af-a802-469c-9505-5ca91e03b354"`
+}
+
+func (LinkModel) CreateLinkBook(link LinkBookRes) (*LinkBookRes, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	_, err := db.LinkCollection.InsertOne(ctx, req)
+	result, err := db.LinkCollection.InsertOne(ctx, link)
 	if err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	link.ID = result.InsertedID.(primitive.ObjectID)
+
+	return &link, nil
 }

fd130be202488da8b900323a4a2fc03801312a9d
gitdog01 <hostdog01@naver.com>
2023-07-06 00:34:01 +0900

    add me

--- backend/app/user/user_usecase.go
+++ backend/app/user/user_usecase.go
@@ -4,8 +4,16 @@ type UserUsecase struct {
 	userModel UserModel
 }
 
-func (u UserUsecase)GetUserByEmail(email string) (*User, error) {
+func (u UserUsecase) GetUserByEmail(email string) (*User, error) {
 	user, err := u.userModel.FindUserByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	return user, err
+}
+
+func (u UserUsecase) GetUserById(id string) (*User, error) {
+	user, err := u.userModel.FindUserById(id)
 	if err != nil {
 		return nil, err
 	}

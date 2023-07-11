fb66135c99418ae508230d08d9cd683ab8c81779
gitdog01 <hostdog01@naver.com>
2023-07-11 20:25:06 +0900

    link data type change

--- backend/app/link/link_model.go
+++ backend/app/link/link_model.go
@@ -17,10 +17,12 @@ type Link struct {
 	UserID     string    `bson:"user_id" json:"userId"`
 	Title      string    `bson:"title" json:"title"`
 	LinkBookId string    `bson:"link_book_id" json:"linkBookId"`
+	Thumbnail  string    `bson:"thumbnail_url" json:"thumbnailURL"`
+	Tags       []string  `bson:"tags" json:"tags"`
 	ReadCount  int       `bson:"read_count" json:"readCount"`
-	LastReadAt time.Time `bson:"last_read_at" json:"LastReadAt"`
-	CreatedAt  time.Time `bson:"created_at" json:"CreatedAt"`
-	UpdatedAt  time.Time `bson:"updated_at" json:"UpdatedAt"`
+	LastReadAt time.Time `bson:"last_read_at" json:"lastReadAt"`
+	CreatedAt  time.Time `bson:"created_at" json:"createdAt"`
+	UpdatedAt  time.Time `bson:"updated_at" json:"updatedAt"`
 }
 
 type LinkModel struct {
@@ -204,7 +206,7 @@ func (LinkModel) UpdateLinkBookIdByLinkId(linkId string, linkBookId string) erro
 	return err
 }
 
-func (LinkModel) UpdateTitleAndUrlByLinkId(linkId string, url string, title string) error {
+func (LinkModel) UpdateTitleAndUrlByLinkId(linkId string, url string, title string, thumbnail string, tags []string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -216,6 +218,14 @@ func (LinkModel) UpdateTitleAndUrlByLinkId(linkId string, url string, title stri
 
 	if title != "" {
 		updateFields["title"] = title
+	}
+
+	if thumbnail != "" {
+		updateFields["thumbnail"] = thumbnail
+	}
+
+	if len(tags) != 0 {
+		updateFields["tags"] = tags
 	}
 
 	if len(updateFields) == 0 {

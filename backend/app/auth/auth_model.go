ce0d480c7b258d71562a00da827df3de3c5c37c2
gitdog01 <hostdog01@naver.com>
2023-07-26 09:32:25 +0900

    fix age int byte

--- backend/app/auth/auth_model.go
+++ backend/app/auth/auth_model.go
@@ -1,11 +1,11 @@
 package auth
 
 type SignUpReq struct {
-	IdToken string `json:"idToken"`
-	Social      string `json:"social"  example:"google"`
-	Nickname    string `json:"nickname"`
-	Gender      string `json:"gender" example:"m"`
-	Age         uint8  `json:"age" example:"20"`
+	IdToken  string `json:"idToken"`
+	Social   string `json:"social"  example:"google"`
+	Nickname string `json:"nickname"`
+	Gender   string `json:"gender" example:"m"`
+	Age      uint32 `json:"age" example:"20"`
 }
 
 type SignUpInfo struct {
@@ -13,6 +13,6 @@ type SignUpInfo struct {
 	Social     string `json:"social"  example:"google"`
 	Nickname   string `json:"nickname"`
 	Gender     string `json:"Gender" example:"m"`
-	Age        uint8  `json:"age" example:"20"`
+	Age        uint32 `json:"age" example:"20"`
 	SignUpDate string `json:"sign_up_date"`
 }
